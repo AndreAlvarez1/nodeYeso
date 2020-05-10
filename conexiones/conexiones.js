9c5464d6978cf34e6242a40233379d07dd840c1f
Andre Alvarez <hey@andrealvarez.com>
2020-05-10 18:35:54 +0200

    subir

--- conexiones/conexiones.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.YESO = {
-    "host": "gator3048.hostgator.com",
-    "user": "andrealv_yeso",
-    "password": "afrodita12",
-    "database": "andrealv_yeso",
-    "port": "3306"
-};
