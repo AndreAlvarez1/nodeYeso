cfe5ed18b04e7241e2fcda631cd19be58e054007
André Alvarez <hey@andrealvarez.com>
2021-06-25 05:54:05 -0400

    merge

--- dist/classes/server.js
+++ dist/classes/server.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.conex = void 0;
 const express_1 = __importDefault(require("express"));
 const environment_1 = require("../global/environment");
 const socket_io_1 = __importDefault(require("socket.io"));
